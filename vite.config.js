da05e489f3744b2ef344fe895ae435d91d44f5e7
Elmar Ismayilov <bamazy@gmail.com>
2023-01-03 16:39:59 +0800

    output folder added

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+        root: 'src',
+        build: {
+            outDir: '../dist'
+        }
 })
 
